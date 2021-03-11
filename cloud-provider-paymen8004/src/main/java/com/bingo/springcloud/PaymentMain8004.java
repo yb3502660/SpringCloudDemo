f7f5853bd6216e120d944218f484df97b0b4683c
yaobin <864665226@qq.com>
2021-03-12 00:27:49 +0800

    day3 Eureka & Discovery

---
+++ cloud-provider-paymen8004/src/main/java/com/bingo/springcloud/PaymentMain8004.java
@@ -0,0 +1,18 @@
+package com.bingo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author:yaobin
+ * @date:2021/3/11,23:06
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class, args);
+    }
+}
---
+++ cloud-provider-paymen8004/src/main/java/com/bingo/springcloud/controller/PaymentController.java
@@ -0,0 +1,29 @@
+package com.bingo.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author:yaobin
+ * @date:2021/3/9,0:44
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+//
+//    @Resource
+//    private DiscoveryClient discoveryClient;
+
+
+    @GetMapping("/payment/zk")
+    public String paymentzk(){
+        return "springcloud with zookeeper: " +serverPort+"\t"+ UUID.randomUUID().toString();
+
+    }
+}
